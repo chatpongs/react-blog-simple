73e57467a36a77c39239db15a6fe920c7b746ce1
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-09-28 17:32:08 +0700

    add content to about screen

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 class About extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        <div>About</div>
+        <div className="container">
+          <div className="my-2"><img src="https://fakeimg.pl/1600x900/?text=Map" className="img-fluid"/></div>
+          <div className="row my-2">
+            <div className="col-md-6"><img src="https://fakeimg.pl/1600x900/?text=Address" className="img-fluid"/></div>
+            <div className="col-md-6"><img src="https://fakeimg.pl/1600x900/?text=Contact Form" className="img-fluid"/></div>
+          </div>
+        </div>
+        <Footer />
       </div>
     )
   }
