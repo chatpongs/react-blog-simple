22ddc4c755bbf9643f22fbdc5c6310e22ff59a74
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-10-04 14:08:09 +0700

    fetch posts with axios

--- src/screens/Blog.js
+++ src/screens/Blog.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import axios from 'axios';
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 class Blog extends React.Component {
+  async componentDidMount() {
+    const posts = await axios.get('https://my-strapi-demo.herokuapp.com/post');
+    posts.data.forEach(post => {
+      console.log(post.title);
+    });
+  }
   render() {
     return (
       <div>
