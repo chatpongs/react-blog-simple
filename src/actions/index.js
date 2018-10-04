20004cd54f388fd49e872648b427fcf0a223122e
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-10-04 17:27:52 +0700

    apply redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,10 @@
 export const getPosts = () => {
-  return {
-    type: 'GET_POSTS',
-    payload: [{id: 1, title: 'Title1', content: 'Content1'}, {id: 2, title: 'Title2', content: 'Content2'}],
-  }
+  return (
+    dispatch => {
+      dispatch({
+        type: 'GET_POSTS',
+        payload: [{id: 1, title: 'Title1', content: 'Content1'}, {id: 2, title: 'Title2', content: 'Content2'}],
+      })
+    }
+  )
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import reducers from './reducers';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
